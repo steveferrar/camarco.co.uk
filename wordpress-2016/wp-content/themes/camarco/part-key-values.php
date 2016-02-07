0a3a720dfd9c2ea1c90763753eb9810d7d3787c3
Steve Ferrar <steveferrar@gmail.com>
2016-02-07 09:20:34 +0000

    Tweaks

--- wordpress-2016/wp-content/themes/camarco/part-key-values.php
+++ wordpress-2016/wp-content/themes/camarco/part-key-values.php
@@ -50,3 +50,21 @@ if(get_field('key_values')):
 <?php 
 endif; 
 ?>
+
+<section class="page">
+
+	<div class="container">
+	
+		<div class="row">
+		
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+	<div class="clearfix"></div>
+	
+</section>
